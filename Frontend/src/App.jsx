b15790a2c5581131510945a2fab709e9b0636ccd
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2026-09-06 11:05:20 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center h-screen text-3xl font-bold'>
+      Authentication System
+    </div>
+  )
+}
+
+export default App
